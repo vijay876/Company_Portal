1b55cba71e4d54fd137bc257d79d30e795283f1f
vijay <vijaykumar6566@gmail.com>
2021-07-08 18:56:53 +0530

    Commited Company_Portal project

---
+++ Company_Portal/src/main/java/com/Company_Portal/CompanyPortalApplication.java
@@ -0,0 +1,13 @@
+package com.Company_Portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyPortalApplication.class, args);
+	}
+
+}
