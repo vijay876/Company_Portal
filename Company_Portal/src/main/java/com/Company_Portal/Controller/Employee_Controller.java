8bed89a27a17301d1daa8e5c9cb3b615ffb29c72
vijay <vijaykumar6566@gmail.com>
2021-07-09 13:56:36 +0530

    done changes

--- Company_Portal/src/main/java/com/Company_Portal/Controller/Employee_Controller.java
+++ Company_Portal/src/main/java/com/Company_Portal/Controller/Employee_Controller.java
@@ -1,5 +1,23 @@
 package com.Company_Portal.Controller;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.Company_Portal.beans.Employee;
+
+@RestController
+@RequestMapping("/Company_Portal_API")
 public class Employee_Controller {
 
+	@GetMapping("/getEmployeeDetails")
+	public List<Employee> getEmployeeDetails() {
+
+		List<Employee> empList = Arrays.asList(new Employee(1, "vijay"), new Employee(2, "Ajay"),
+				new Employee(3, "Uday"));
+		return empList;
+	}
 }
---
+++ Company_Portal/src/main/java/com/Company_Portal/beans/Employee.java
@@ -0,0 +1,29 @@
+package com.Company_Portal.beans;
+
+public class Employee {
+
+	private int eno;
+	private String name;
+
+	public Employee(int eno, String name) {
+		this.eno = eno;
+		this.name = name;
+	}
+
+	public int getEno() {
+		return eno;
+	}
+
+	public void setEno(int eno) {
+		this.eno = eno;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
